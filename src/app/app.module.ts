f291b65f237056cf9903a015ceddd46352727f3c
dogukanakay <akay.dogukann@gmail.com>
2023-04-18 02:55:57 +0300

    Firstday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RentalComponent,
+    CarComponent,
+    ColorComponent,
+    BrandComponent,
+    CustomerComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+import { Time } from "@angular/common"
+
+export interface Rental{
+    brandName:string
+    customerName:string
+    rentDate:Time
+    returnDate:Time
+}
